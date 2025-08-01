f5433fe70ccf0d23b6297cd25ffe695f1118366c
Eduardo Jimenez <eddiejdev@gmail.com>
2025-08-01 15:33:55 -0700

    fix in authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,20 @@
+// backend/middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../config');
+// Adjust path: two levels up to the root config.js
+const { JWT_SECRET } = require('../../config');
 
-module.exports = function (req, res, next) {
+module.exports = function authenticate(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    return res.sendStatus(401);
+  }
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      return res.sendStatus(403);
+    }
     req.user = user;
     console.log('Decoded JWT user:', req.user);
     next();
