c89b491ee444da8198f6c18ac7f8316065769526
Eduardo Jimenez <eddiejdev@gmail.com>
2025-08-22 23:19:45 -0700

    fix: correct login logic in authController and add test route

--- backend/middleware/validationMiddleware.js
+++ backend/middleware/validationMiddleware.js
@@ -4,8 +4,9 @@ const { validationResult } = require('express-validator');
 exports.handleValidation = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const [{ msg, param }] = errors.array();
-    return res.status(400).json({ message: `${param}: ${msg}` });
+    // The error object key is 'path', not 'param'. This change fixes the error message.
+    const [{ msg, path }] = errors.array();
+    return res.status(400).json({ message: `${path}: ${msg}` });
   }
   next();
 };
