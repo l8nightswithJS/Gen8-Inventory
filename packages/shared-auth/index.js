14737911483483b36ce25ce7ea4660fd7d63dd1f
Eduardo Jimenez <eddiejdev@gmail.com>
2025-09-01 16:59:16 -0700

    fixes: import handleValidation from shared-auth package

--- packages/shared-auth/index.js
+++ packages/shared-auth/index.js
@@ -2,7 +2,7 @@ const verifyJwt = require('./verifyJwt');
 const authMiddleware = require('./authMiddleware');
 const requireRole = require('./requireRole');
 const requireClientMatch = require('./requireClientMatch');
-const handleValidation = require('./validationMiddleware');
+const { handleValidation } = require('./validationMiddleware');
 
 module.exports = {
   verifyJwt,
