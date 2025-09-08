0dbb61e21b669ad615367fc3abc72a5b9a91e95c
Eduardo Jimenez <eddiejdev@gmail.com>
2025-09-07 22:56:17 -0700

    add route to gateway

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -91,6 +91,10 @@ app.use(
 
 // Client service
 app.use('/api/clients', prox(CLIENT_URL));
+app.use(
+  '/api/clients/add',
+  prox(CLIENT_URL, { pathRewrite: { '^/api/clients/add': '/add' } }),
+);
 
 // Barcode service
 app.use(
