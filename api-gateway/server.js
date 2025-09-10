0f4f9533e913121ad47785b0f3d5e861744c0661
Eduardo Jimenez <eddiejdev@gmail.com>
2025-09-09 21:20:54 -0700

    fix server

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -78,10 +78,7 @@ const prox = (target, options = {}) =>
 app.use('/api/auth', prox(AUTH_URL, { pathRewrite: { '^/api/auth': '' } }));
 
 // User management (admin only)
-app.use(
-  '/api/users',
-  prox(AUTH_URL, { pathRewrite: { '^/api/users': '/users' } }),
-);
+app.use('/api/users', prox(AUTH_URL, { pathRewrite: { '^/api/users': '' } }));
 
 // Inventory service
 app.use(
